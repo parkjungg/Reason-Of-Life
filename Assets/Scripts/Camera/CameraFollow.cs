d5e19d19d324054114eda5f3610d2eb00c2e5601
parkjungg <pjk3665@naver.com>
2026-05-17 20:29:50 +0900

    feat : attatch to camera in player

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private void LateUpdate()
+    {
+        if (target == null)
+            return;
+
+        transform.position = new Vector3(
+            target.position.x,
+            target.position.y,
+            transform.position.z
+            );
+    }
+}
