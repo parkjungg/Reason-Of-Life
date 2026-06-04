ec94ccc81eb944c115d30be43eee5c2ab0e30ea6
parkjungg <pjk3665@naver.com>
2026-06-04 17:02:47 +0900

    chore : manage maxAPByPhase in define.cs

--- Assets/Scripts/Core/Define.cs
+++ Assets/Scripts/Core/Define.cs
@@ -10,11 +10,21 @@ public enum LifePhase
 
 public static class GameConfig
 {
+    // 페이즈 진행일 수
     public static readonly Dictionary<LifePhase, int> PhaseDuration = new()
     {
         { LifePhase.Infancy, 2 },
         { LifePhase.Adolescence, 3 },
         { LifePhase.Adult, 3 },
         { LifePhase.Elderly, 2 }
+    };
+    
+    // 각 페이즈 기본 행동력
+    public static readonly Dictionary<LifePhase, int> MaxAPByPhase = new()
+    {
+        { LifePhase.Infancy, 5 },
+        { LifePhase.Adolescence, 10 },
+        { LifePhase.Adult, 10 },
+        { LifePhase.Elderly, 7 }
     };
 }
