7dcaf49a23e182dcdffdc30bd06f55c0e246398c
parkjungg <pjk3665@naver.com>
2026-06-12 13:14:14 +0900

    feat : happy state UI animation

--- Assets/Scripts/Animation/PlayerAnimator.cs
+++ Assets/Scripts/Animation/PlayerAnimator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,28 @@ using UnityEngine;
 public class PlayerAnimator : MonoBehaviour
 {
     private Animator animator;
+    [SerializeField] private Animator happinessAnimator;
+    
     private Vector2 lastDirection = Vector2.down;
 
     private static readonly int MoveX = Animator.StringToHash("moveX");
     private static readonly int MoveY = Animator.StringToHash("moveY");
     private static readonly int IsMoving = Animator.StringToHash("isMoving");
+
+    private static readonly int HappinessState = Animator.StringToHash("happinessState");
     
     void Awake() => animator = GetComponent<Animator>();
+
+    private void Start()
+    {
+        HappinessManager.instance.OnStateChanged += UpdateHappinessAnimation;
+        UpdateHappinessAnimation(HappinessManager.instance.CurrentState);
+    }
+
+    void OnDestroy()
+    {
+        HappinessManager.instance.OnStateChanged -= UpdateHappinessAnimation;
+    }
 
     public void UpdateAnimation(Vector2 direction, bool isMoving)
     {
@@ -20,5 +36,20 @@ public class PlayerAnimator : MonoBehaviour
         animator.SetFloat(MoveX, lastDirection.x);
         animator.SetFloat(MoveY, lastDirection.y);
         animator.SetBool(IsMoving, isMoving);
+    }
+
+    void UpdateHappinessAnimation(global::HappinessState state)
+    {
+        int index = state switch
+        {
+            global::HappinessState.Happy => 0,
+            global::HappinessState.Stable => 1,
+            global::HappinessState.Bad => 2,
+            global::HappinessState.Depressed => 2,
+            global::HappinessState.Collapse => 3,
+            _ => 1
+        };
+        Debug.Log($"[Anim] 행복도 상태: {state} → index: {index}");
+        happinessAnimator.SetInteger(HappinessState, index);
     }
 }
